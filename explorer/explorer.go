ea8ba95ed653d82bc4d088b9bde81e97f2708d54
Deployer <drivespublic@gmail.com>
2021-07-01 09:20:01 +0900

    note PC

---
+++ explorer/explorer.go
@@ -0,0 +1,52 @@
+package explorer
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/goLangCoin/blockchain"
+)
+
+var templates *template.Template
+
+const (
+	PORT        string = ":4000"
+	templateDir string = "explorer/templates/"
+)
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	// template.Must => err를 체크해줌
+	// tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
+	data := homeData{"Home", blockchain.AllBlocks()}
+	templates.ExecuteTemplate(rw, "home.gohtml", data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		// Form 값을 채워줌
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	// 이 핸들러가 동작하기 전에 gohtml은 rendering이 되어있어야 한다.
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost%s\n", PORT)
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
--- main.go
+++ main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"text/template"
-
-	"github.com/goLangCoin/blockchain"
-)
-
-const PORT string = ":4000"
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	// template.Must => err를 체크해줌
-	tmpl := template.Must(template.("templates/pages/home.gohtml"))
-	data := homeData{"Home", blockchain.AllBlocks()}
-	tmpl.Execute(rw, data)
-}
+import "github.com/goLangCoin/explorer"
 
 func main() {
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http://localhost%s\n", PORT)
-	log.Fatal(http.ListenAndServe(PORT, nil))
+	explorer.Start()
 }
